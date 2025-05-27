c5906986d3079ab933a2a896281571cce54a0982
Rafael mesquita <rafaelmesquitarr5@gmail.com>
2025-05-26 22:32:19 -0300

    Adicionando a pagina cadastro

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for,redirect,request
 from models.database import db, Paciente
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///clinica.db'
@@ -13,8 +13,17 @@ def loginPage():
 def homePage():
     return render_template('home.html')
 
+@app.route('/cadastro', methods = ['GET','POST'])
+def cadastroPage():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        idade = request.form['idade']
+        especialidade = request.form['especialidade']
+        return redirect(url_for('home'))
+    return render_template('cadastro.html')
+
 with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
